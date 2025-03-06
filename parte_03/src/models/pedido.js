baf3448675a0e71a99e22771d67753c4d07a18e2
Gustavo Navarro <gusta@gusta.>
2025-03-06 16:03:57 -0300

    feat: adiciona os modelos do banco de dados

---
+++ parte_03/src/models/pedido.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const Pedido = sequelize.define('Pedido', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    id_cliente: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Clientes',
+            key: 'id'
+        },
+        allowNull: false,
+    },
+    data_pedido: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    total: {
+        type: DataTypes.FLOAT
+    }
+});
+
+export default Pedido;
